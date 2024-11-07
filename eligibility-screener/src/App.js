8291cad3572f32b670ff4e1669459f353486a5c6
Sarah <klute.s@northeastern.edu>
2024-11-07 10:50:03 -0500

    most recent full updates

--- eligibility-screener/src/App.js
+++ eligibility-screener/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import EligibilitySurvey from "./EligibilitySurvey"; // Adjust the path as necessary
 
-console.log("EligibilitySurvey component:", EligibilitySurvey);
+console.log("Eligibility Survey component 2:", EligibilitySurvey);
 function App() {
   return (
     <div>
